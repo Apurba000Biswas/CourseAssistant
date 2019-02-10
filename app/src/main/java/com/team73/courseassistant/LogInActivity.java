6b0a19afda6f653dc95ef8729dc1fa2ef2153d39
Apurba <apurba10024@gmail.com>
2019-02-11 03:15:16 +0600

    1.5 Updated Login style

--- app/src/main/java/com/team73/courseassistant/LogInActivity.java
+++ app/src/main/java/com/team73/courseassistant/LogInActivity.java
@@ -6,8 +6,11 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -32,6 +35,8 @@ public class LogInActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_log_in);
 
         resources = getResources();
+        LinearLayout inputHolder = findViewById(R.id.input_holder);
+        inputHolder.setVisibility(View.GONE);
         setGoogle();
     }
 
@@ -52,16 +57,21 @@ public class LogInActivity extends AppCompatActivity implements
 
     public void signInClicked(View view) {
         TextView stateTextView = findViewById(R.id.tv_state);
-        if (Network.isConnectedToInternet(this)){
-            // connect to Google server to log in
-            stateTextView.setVisibility(View.GONE);
-            if (google != null){
-                Intent intent = Auth.GoogleSignInApi.getSignInIntent(google);
-                startActivityForResult(intent, REQ_CODE_GOOGLE_SIGN_IN);
+        String text = ((TextView)findViewById(R.id.tv_sign_in)).getText().toString();
+        if (TextUtils.equals(text, resources.getString(R.string.text_sign_in))){
+            if (Network.isConnectedToInternet(this)){
+                // connect to Google server to log in
+                stateTextView.setVisibility(View.GONE);
+                if (google != null){
+                    Intent intent = Auth.GoogleSignInApi.getSignInIntent(google);
+                    startActivityForResult(intent, REQ_CODE_GOOGLE_SIGN_IN);
+                }
+            }else{
+                stateTextView.setVisibility(View.VISIBLE);
+                stateTextView.setText(resources.getString(R.string.no_internet));
             }
-        }else{
-            stateTextView.setVisibility(View.VISIBLE);
-            stateTextView.setText(resources.getString(R.string.no_internet));
+        }else if (TextUtils.equals(text, resources.getString(R.string.add_profile))){
+            Toast.makeText(this, "OK Profile Added", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -78,6 +88,11 @@ public class LogInActivity extends AppCompatActivity implements
                 String msg = resources.getString(R.string.sign_in_success)
                         + " " + acct.getEmail();
                 stateTextView.setText(msg);
+
+                LinearLayout inputHolder = findViewById(R.id.input_holder);
+                inputHolder.setVisibility(View.VISIBLE);
+                TextView signInTextView = findViewById(R.id.tv_sign_in);
+                signInTextView.setText(resources.getString(R.string.add_profile));
             } else {
                 stateTextView.setText(resources.getString(R.string.log_in_failed));
             }
