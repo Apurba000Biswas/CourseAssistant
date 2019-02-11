b41876f241c9722172ea9b26fa19ab4f268311ac
Apurba <apurba10024@gmail.com>
2019-02-11 23:21:45 +0600

    1.8 Added MainActivity App bar

--- app/src/main/java/com/team73/courseassistant/activity/MainActivity.java
+++ app/src/main/java/com/team73/courseassistant/activity/MainActivity.java
@@ -1,7 +1,12 @@
 package com.team73.courseassistant.activity;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.team73.courseassistant.R;
 
@@ -11,5 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mTopToolbar = findViewById(R.id.toolbar);
+        mTopToolbar.setTitle("");
+        setSupportActionBar(mTopToolbar);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_search) {
+            Toast.makeText(MainActivity.this, "Action clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
