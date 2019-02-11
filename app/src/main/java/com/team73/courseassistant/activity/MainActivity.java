fb6b5a7271a9a64f4b9f9432049f2f490de1b1b4
Apurba <apurba10024@gmail.com>
2019-02-12 05:07:03 +0600

    2.1 Added Tab icon and Secetion

--- app/src/main/java/com/team73/courseassistant/activity/MainActivity.java
+++ app/src/main/java/com/team73/courseassistant/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.team73.courseassistant.activity;
 
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -11,7 +13,7 @@ import android.view.MenuItem;
 import com.team73.courseassistant.R;
 import com.team73.courseassistant.adapters.courseFragmentPgAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TabLayout.BaseOnTabSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +26,33 @@ public class MainActivity extends AppCompatActivity {
 
         ViewPager viewPager = findViewById(R.id.view_pager);
         courseFragmentPgAdapter adapter =
-                new courseFragmentPgAdapter(getSupportFragmentManager());
+                new courseFragmentPgAdapter(getSupportFragmentManager(), this);
         viewPager.setAdapter(adapter);
 
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+        tabLayout.addOnTabSelectedListener(this);
+
+        setTab(tabLayout);
     }
 
+    private void setTab(TabLayout tabLayout){
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_home_foreground);
+        Drawable drawable = tabLayout.getTabAt(0).getIcon();
+        setSelectedColor(drawable);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_average_foreground);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_profile_foreground);
+    }
+
+    private void setSelectedColor(Drawable drawable){
+        if (drawable != null){
+            drawable.setColorFilter( 0xFF000000, PorterDuff.Mode.MULTIPLY );
+        }
+    }
+
+    private void setUnselectedColor(Drawable drawable){
+        drawable.setColorFilter( 0xffffffff, PorterDuff.Mode.MULTIPLY );
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,7 +68,24 @@ public class MainActivity extends AppCompatActivity {
             onSearchRequested();
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(TabLayout.Tab tab) {
+        Drawable drawable = tab.getIcon();
+        setSelectedColor(drawable);
+    }
+
+    @Override
+    public void onTabUnselected(TabLayout.Tab tab) {
+        Drawable drawable = tab.getIcon();
+        setUnselectedColor(drawable);
+    }
+
+    @Override
+    public void onTabReselected(TabLayout.Tab tab) {
+        Drawable drawable = tab.getIcon();
+        setSelectedColor(drawable);
     }
 }
--- app/src/main/java/com/team73/courseassistant/adapters/courseFragmentPgAdapter.java
+++ app/src/main/java/com/team73/courseassistant/adapters/courseFragmentPgAdapter.java
@@ -1,17 +1,31 @@
 package com.team73.courseassistant.adapters;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
 
+import com.team73.courseassistant.R;
 import com.team73.courseassistant.fragments.AverageFragment;
 import com.team73.courseassistant.fragments.HomeFragment;
 import com.team73.courseassistant.fragments.ProfileFragment;
 
 public class courseFragmentPgAdapter extends FragmentPagerAdapter {
 
-    public courseFragmentPgAdapter(FragmentManager fm) {
+    private Context mContext;
+    /*private int[] imageResId = {
+            R.drawable.ic_home,
+            R.drawable.ic_average,
+            R.drawable.ic_profile
+    };*/
+
+    public courseFragmentPgAdapter(FragmentManager fm, Context context) {
         super(fm);
+        mContext = context;
     }
 
     @Override
@@ -28,18 +42,36 @@ public class courseFragmentPgAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        return 4;
+        return 3;
     }
 
     @Override
     public CharSequence getPageTitle(int position) {
+
+        /*
+        Drawable image;
+        SpannableString sb;
         switch (position){
             case 0:
-                return "Home";
+                image = mContext.getResources().getDrawable(imageResId[position]);
+                sb = new SpannableString("  Home");
+                break;
             case 1:
-                return "Average";
+                image = mContext.getResources().getDrawable(imageResId[position]);
+                sb = new SpannableString("  Average");
+                break;
             default:
-                return "Profile";
+                image = mContext.getResources().getDrawable(imageResId[2]);
+                sb = new SpannableString("  Profile");
+                break;
         }
+        image.setBounds(0, 0, image.getIntrinsicWidth(), image.getIntrinsicHeight());
+        ImageSpan imageSpan = new ImageSpan(image, ImageSpan.ALIGN_BOTTOM);
+        sb.setSpan(imageSpan, 0, 1, Spannable.SPAN_PRIORITY);
+        return sb;
+        */
+        return "";
     }
+
+
 }
